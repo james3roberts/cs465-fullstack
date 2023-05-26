214361cfca6177ac9638d56f55fbc25fe426f2ba
James <james3roberts@gmail.com>
2023-05-26 14:30:35 -0600

    Created Object schema with Mongoose, populate MongoDB

---
+++ .seedgooserc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    modelBaseDirectory: 'app_server/models',
+    models: ['*.js', '!db.js'],
+    data: 'data',
+    db: 'mongodb://127.0.0.1:27017/travlr'
+    //db:'mongodb://localhost:27017/travlr'
+};
+
